4e536f7d44eb15a379035d3e07c66975d2b194cd
cunabula27 <adrian@cunabulapress.com>
2026-01-21 21:33:53 +0000

    Small tidy up and comment out unused var
    Moved the comments so still readable with the code folded.

--- APC40-MkII.js
+++ APC40-MkII.js
@@ -2,44 +2,43 @@ var RBGmodes = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
 
 // All note addresses are in the format [channel, note]
 
-// AG > This variable is all the regular notes
-var noteNote = {
-    clipLaunch:[
-        [1,32], [1,33], [1,34], [1,35], [1,36], [1,37], [1,38], [1,39], // Clip Launch Pads arranged on the APC40
+var noteNote = {                // This variable is all the regular notes
+    clipLaunch:[                // Clip Launch Pads ordered as arranged on the APC40
+        [1,32], [1,33], [1,34], [1,35], [1,36], [1,37], [1,38], [1,39], 
         [1,24], [1,25], [1,26], [1,27], [1,28], [1,29], [1,30], [1,31],
         [1,16], [1,17], [1,18], [1,19], [1,20], [1,21], [1,22], [1,23], 
         [1, 8], [1, 9], [1,10], [1,11], [1,12], [1,13], [1,14], [1,15],  
         [1, 0], [1, 1], [1, 2], [1, 3], [1, 4], [1, 5], [1, 6], [1, 7]
     ],
-    sceneLaunch:[
-        [1,82], // Scene Launch Pads arranged top to bottom
+    sceneLaunch:[               // Scene Launch Pads ordered as arranged on the APC40 top to bottom
+        [1,82], 
         [1,83],
         [1,84],
         [1,85],
         [1,86]
     ],
-    clipStop:[
-        [1,52], [2,52], [3,52], [4,52], [5,52], [6,52], [7,52], [8,52],    [1,81] // Clip Stop 1-8 + Stop All Clips
+    clipStop:[                  // Clip Stop 1-8 + Stop All Clips
+        [1,52], [2,52], [3,52], [4,52], [5,52], [6,52], [7,52], [8,52],    [1,81] 
     ],
-    trackSelector:[
-        [1,51], [2,51], [3,51], [4,51], [5,51], [6,51], [7,51], [8,51],    [1,80]  // Track Selectors 1-8 + Master
+    trackSelector:[             // Track Selectors 1-8 + Master
+        [1,51], [2,51], [3,51], [4,51], [5,51], [6,51], [7,51], [8,51],    [1,80] 
     ],
     trackControl:{
-        mute:[
-            [1,50], [2,50], [3,50], [4,50], [5,50], [6,50], [7,50], [8,50] // Track 1-8 Mute [1-8]
+        mute:[                  // Track 1-8 Mute [1-8]
+            [1,50], [2,50], [3,50], [4,50], [5,50], [6,50], [7,50], [8,50]
         ],
-        solo:[
-            [1,66], [2,66], [3,66], [4,66], [5,66], [6,66], [7,66], [8,66] // Track 1-8 Solo [S]
+        solo:[                  // Track 1-8 Solo [S]
+            [1,66], [2,66], [3,66], [4,66], [5,66], [6,66], [7,66], [8,66] 
         ],
-        crossfadeAssign:[
-            [1,49], [2,49], [3,49], [4,49], [5,49], [6,49], [7,49], [8,49] // Track 1-8 Crossfade Assign [A/B]
+        crossfadeAssign:[       // Track 1-8 Crossfade Assign [A/B]
+            [1,49], [2,49], [3,49], [4,49], [5,49], [6,49], [7,49], [8,49]
         ],
-        recordArm:[
-            [1,48], [2,48], [3,48], [4,48], [5,48], [6,48], [7,48], [8,48] // Track 1-8 Record-Arm [.]
+        recordArm:[             // Track 1-8 Record-Arm [.]
+            [1,48], [2,48], [3,48], [4,48], [5,48], [6,48], [7,48], [8,48]
         ]
     },
-    button:{
-        top:[
+    button:{                    // Button controls on the right-hand side
+        top:[                   // Block of buttons above the Device Knobs
             [1, 87], // Buttons Top - pan
             [1, 88], // Buttons Top - sends
             [1, 89], // Buttons Top - user
@@ -53,7 +52,7 @@ var noteNote = {
             [1,100], // Buttons Top - nudgeMinus
             [1,101]  // Buttons Top - nudgePlus
         ],
-        bottom:[
+        bottom:[                // Block of buttons below the Device Knobs
             [1, 58], // Buttons Bottom - 1DevicePrev
             [1, 59], // Buttons Bottom - 2DeviceNext
             [1, 60], // Buttons Bottom - 3BankPrev
@@ -67,8 +66,7 @@ var noteNote = {
             [1,103]  // Buttons Bottom - bank
         ]
     },
-    // Nb. Bank Select ordered CLOCKWISE FROM TOP
-    bankSelect:[
+    bankSelect:[                // Nb. Bank Select ordered CLOCKWISE FROM TOP
         [1,94], // Bank Select - up	
         [1,96], // Bank Select - right
         [1,95], // Bank Select - down
@@ -76,11 +74,11 @@ var noteNote = {
     ]
 };
 
-var noteCC = {
-    knobTop:[
-        [1,48], [1,49], [1,50], [1,51], [1,52], [1,53], [1,54], [1,55] // Assignable Knobs (Top)
+var noteCC = {                  // Control Change Notes
+    knobTop:[                   // Assignable Knobs (Top)
+        [1,48], [1,49], [1,50], [1,51], [1,52], [1,53], [1,54], [1,55]
     ],
-    knobRight:[
+    knobRight:[                 // Device Knobs (Right)
         [1,16], [1,17], [1,18], [1,19], [1,20], [1,21], [1,22], [1,23], // Device Knobs (Right) - All Modes
         [2,16], [2,17], [2,18], [2,19], [2,20], [2,21], [2,22], [2,23], // Device Knobs (Right) - Generic Mode (0) ONLY
         [3,16], [3,17], [3,18], [3,19], [3,20], [3,21], [3,22], [3,23], // Device Knobs (Right) - Generic Mode (0) ONLY
@@ -91,12 +89,12 @@ var noteCC = {
         [8,16], [8,17], [8,18], [8,19], [8,20], [8,21], [8,22], [8,23], // Device Knobs (Right) - Generic Mode (0) ONLY
         [9,16], [9,17], [9,18], [9,19], [9,20], [9,21], [9,22], [9,23]  // Device Knobs (Right) - Generic Mode (0) ONLY
     ],
-    encoder:[
+    encoder:[                   // Rotary Encoders
         [1,47], // Rotary Encoder - cue
         [1,13]  // Rotary Encoder - tempo
     ],
-    fader:[
-        [1,7], [2,7], [3,7], [4,7], [5,7], [6,7], [7,7], [8,7], [1,14], [1,15] // Faders Track 1-8 + Master + Crossfader
+    fader:[                     // Faders Track 1-8 + Master + Crossfader
+        [1,7], [2,7], [3,7], [4,7], [5,7], [6,7], [7,7], [8,7],     [1,14],     [1,15]
     ]
 };
 			
@@ -203,7 +201,7 @@ var ccNotes = {
 */
 
 // AG > This is all the names of the buttons that light up
-var buttonLedKeys = ["pan","sends","user","play","record","session","metronome", "1DevicePrev", "2DeviceNext", "3BankPrev", "4BankNext", "5DevOnOff", "6DevLock", "7ClipDevView", "8DetailView", "bank"];
+// var buttonLedKeys = ["pan","sends","user","play","record","session","metronome", "1DevicePrev", "2DeviceNext", "3BankPrev", "4BankNext", "5DevOnOff", "6DevLock", "7ClipDevView", "8DetailView", "bank"];
 
 var noteValueObj = [[],[],[],[],[],[],[],[],[]]; // AG > EMPTY ARRAY DECLARATION FOR MIDI MESSAGES ???
 
