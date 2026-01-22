645c10d945490d7dcc6b94c81781f2322b3e562b
cunabula27 <adrian@cunabulapress.com>
2026-01-22 15:30:51 +0000

    Added ability to set LED ring mode for all knobs + flattened Object structure
    Instead of having two sub objects for the buttons (top/bottom) and the knobs (top/right) I've merged both of both sets together into one as they each do the same thing, as opposed to the Clip Launch and Scene pads which look the same and have the same parameters but do different things on the controller. Documented in the comments what the relevant indices are. Slightly less readable code but simpler to implement.
    
    Also renamed Assignable Knobs to Track Knobs. Makes no difference and the MIDI spec doc uses both - helpful.

--- APC40-MkII.js
+++ APC40-MkII.js
@@ -39,35 +39,33 @@ var noteNote = {                // This variable is all the regular notes
             [1,48], [2,48], [3,48], [4,48], [5,48], [6,48], [7,48], [8,48]
         ]
     },
-    button:{                    // Button controls on the right-hand side
-        top:[                   // Block of buttons above the Device Knobs
-            [1, 87], // Buttons Top - pan
-            [1, 88], // Buttons Top - sends
-            [1, 89], // Buttons Top - user
+    button:[                    // Button controls 
+            [1, 87], //  [0] Buttons Top - pan
+            [1, 88], //  [1] Buttons Top - sends
+            [1, 89], //  [2] Buttons Top - user
 
-            [1, 91], // Buttons Top - play
-            [1, 93], // Buttons Top - record
-            [1,102], // Buttons Top - session
+            [1, 91], //  [3] Buttons Top - play
+            [1, 93], //  [4] Buttons Top - record
+            [1,102], //  [5] Buttons Top - session
 
-            [1, 90], // Buttons Top - metronome
-            [1, 99], // Buttons Top - tapTempo
-            [1,100], // Buttons Top - nudgeMinus
-            [1,101]  // Buttons Top - nudgePlus
+            [1, 90], //  [6] Buttons Top - metronome
+            [1, 99], //  [7] Buttons Top - tapTempo
+            [1,100], //  [8] Buttons Top - nudgeMinus
+            [1,101], //  [9] Buttons Top - nudgePlus
+
+            [1, 58], // [10] Buttons Bottom - 1DevicePrev
+            [1, 59], // [11] Buttons Bottom - 2DeviceNext
+            [1, 60], // [12] Buttons Bottom - 3BankPrev
+            [1, 61], // [13] Buttons Bottom - 4BankNext
+
+            [1, 62], // [14] Buttons Bottom - 5DevOnOff
+            [1, 63], // [15] Buttons Bottom - 6DevLock
+            [1, 64], // [16] Buttons Bottom - 7ClipDevView
+            [1, 65], // [17] Buttons Bottom - 8DetailView
+
+            [1, 98], // [18] Buttons Bottom - shift 
+            [1,103], // [19] Buttons Bottom - bank
         ],
-        bottom:[                // Block of buttons below the Device Knobs
-            [1, 58], // Buttons Bottom - 1DevicePrev
-            [1, 59], // Buttons Bottom - 2DeviceNext
-            [1, 60], // Buttons Bottom - 3BankPrev
-            [1, 61], // Buttons Bottom - 4BankNext
-            [1, 62], // Buttons Bottom - 5DevOnOff
-            [1, 63], // Buttons Bottom - 6DevLock
-            [1, 64], // Buttons Bottom - 7ClipDevView
-            [1, 65], // Buttons Bottom - 8DetailView
-
-            [1, 98], // Buttons Bottom - shift 
-            [1,103]  // Buttons Bottom - bank
-        ]
-    },
     bankSelect:[                // Nb. Bank Select ordered CLOCKWISE FROM TOP
         [1,94], // Bank Select - up	
         [1,96], // Bank Select - right
@@ -77,19 +75,18 @@ var noteNote = {                // This variable is all the regular notes
 };
 
 var noteCC = {                  // Control Change Notes
-    knobTop:[                   // Assignable Knobs (Top)
-        [1,48], [1,49], [1,50], [1,51], [1,52], [1,53], [1,54], [1,55]
-    ],
-    knobRight:[                 // Device Knobs (Right)
-        [1,16], [1,17], [1,18], [1,19], [1,20], [1,21], [1,22], [1,23], // Device Knobs (Right) - All Modes
-        [2,16], [2,17], [2,18], [2,19], [2,20], [2,21], [2,22], [2,23], // Device Knobs (Right) - Generic Mode (0) ONLY
-        [3,16], [3,17], [3,18], [3,19], [3,20], [3,21], [3,22], [3,23], // Device Knobs (Right) - Generic Mode (0) ONLY
-        [4,16], [4,17], [4,18], [4,19], [4,20], [4,21], [4,22], [4,23], // Device Knobs (Right) - Generic Mode (0) ONLY
-        [5,16], [5,17], [5,18], [5,19], [5,20], [5,21], [5,22], [5,23], // Device Knobs (Right) - Generic Mode (0) ONLY
-        [6,16], [6,17], [6,18], [6,19], [6,20], [6,21], [6,22], [6,23], // Device Knobs (Right) - Generic Mode (0) ONLY
-        [7,16], [7,17], [7,18], [7,19], [7,20], [7,21], [7,22], [7,23], // Device Knobs (Right) - Generic Mode (0) ONLY
-        [8,16], [8,17], [8,18], [8,19], [8,20], [8,21], [8,22], [8,23], // Device Knobs (Right) - Generic Mode (0) ONLY
-        [9,16], [9,17], [9,18], [9,19], [9,20], [9,21], [9,22], [9,23]  // Device Knobs (Right) - Generic Mode (0) ONLY
+    knob:[                      // All Knobs
+        [1,48], [1,49], [1,50], [1,51], [1,52], [1,53], [1,54], [1,55], // Track Knobs (Top) [0]-[7]
+                                // Device Knobs (Right)
+        [1,16], [1,17], [1,18], [1,19], [1,20], [1,21], [1,22], [1,23], // Track 1 / All Modes              [8] - [15]
+        [2,16], [2,17], [2,18], [2,19], [2,20], [2,21], [2,22], [2,23], // Track 2 - Generic Mode (0) ONLY [16] - [23]
+        [3,16], [3,17], [3,18], [3,19], [3,20], [3,21], [3,22], [3,23], // Track 3 - Generic Mode (0) ONLY [24] - [31]
+        [4,16], [4,17], [4,18], [4,19], [4,20], [4,21], [4,22], [4,23], // Track 4 - Generic Mode (0) ONLY [32] - [39]
+        [5,16], [5,17], [5,18], [5,19], [5,20], [5,21], [5,22], [5,23], // Track 5 - Generic Mode (0) ONLY [40] - [47]
+        [6,16], [6,17], [6,18], [6,19], [6,20], [6,21], [6,22], [6,23], // Track 6 - Generic Mode (0) ONLY [48] - [55]
+        [7,16], [7,17], [7,18], [7,19], [7,20], [7,21], [7,22], [7,23], // Track 7 - Generic Mode (0) ONLY [56] - [63]
+        [8,16], [8,17], [8,18], [8,19], [8,20], [8,21], [8,22], [8,23], // Track 8 - Generic Mode (0) ONLY [64] - [71]
+        [9,16], [9,17], [9,18], [9,19], [9,20], [9,21], [9,22], [9,23]  // Master  - Generic Mode (0) ONLY [72] - [79]
     ],
     encoder:[                   // Rotary Encoders
         [1,47], // Rotary Encoder - cue
@@ -225,45 +222,26 @@ function setAPC40mode(mode) { // Reset Controller
     script.log(mode);
 };
 
-/*
-function setLEDringMode(Group, Mode) {
-
-    if(Group == 0){
-
-    } else if(group == 1){
-
-    } else if(group == 2){
-
-    }
-    parameters.lights.ledRingModes.assignableKnobs_Top_.1
-
-}
-
-var x = local.parameters.lights.rgbPadMode_Colour.clipLaunchPadColour.layer1_Clip1;
-
-function moduleParameterChanged(x)
-{
-	if(param.isParameter())
-	{
-		script.log("Module parameter changed : "+param.name+" > "+param.get());
-	}else 
-	{
-		script.log("Module parameter triggered : "+param.name);	
-	}
-}
+function setLEDringMode(group, mode) {
+    for (var i = group[0]; i < group[1]+1; i++) {
+        local.sendCC(noteCC.knob[i][0], 8+noteCC.knob[i][1], mode);
+    };
+};
 
 
-function testRGB(x){
-    local.sendNoteOn(1, 1, x);
+
+function testRGB(){
+    var x = local.parameters.lights.rgbPadMode_Colour.clipLaunchPadColour.layer1_Clip1.get();
+    local.sendNoteOn(1, 0, x);
     script.log(x);
 };
 
-testRGB();
-
-*/
+/* Put away for now:
 
 function testAddress(){
     script.log(noteNote.clipLaunch[6][1]);
 };
 
 testAddress();
+
+*/
