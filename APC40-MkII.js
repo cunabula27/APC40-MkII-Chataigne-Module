cb9a4e57cf479c261899bbf90d6dc02942448294
cunabula27 <adrian@cunabulapress.com>
2026-01-27 20:24:46 +0000

    Binned last of the original code I was referencing as I've not ended up using any of it

--- APC40-MkII.js
+++ APC40-MkII.js
@@ -65,131 +65,20 @@ var noteCC = {                              // Control Change Notes
 
     fader:[7, 14, 15]                       // Faders Track [1-8, 7] + Master [1,14] + Crossfader [1,15]
 };
-			
-/* ORIGINAL NOTE VARIABLE
-
-    pads: {
-        main: // AG > These are all the RGB Pads including the Scene Launch Pads in the final column
-        [ 
-			[[1,32], [1,33], [1,34], [1,35], [1,36], [1,37], [1,38], [1,39],    [1,82]], // Layer 5
-			[[1,24], [1,25], [1,26], [1,27], [1,28], [1,29], [1,30], [1,31],    [1,83]], // Layer 4 
-			[[1,16], [1,17], [1,18], [1,19], [1,20], [1,21], [1,22], [1,23],    [1,84]], // Layer 3
-			[[1, 8], [1, 9], [1,10], [1,11], [1,12], [1,13], [1,14], [1,15],    [1,85]], // Layer 2
-			[[1, 0], [1, 1], [1, 2], [1, 3], [1, 4], [1, 5], [1, 6], [1, 7],    [1,86]], // Layer 1
-
-			[[1,52], [2,52], [3,52], [4,52], [5,52], [6,52], [7,52], [8,52],    [1,81]], // Clip Stop and Stop All Clips
-
-			[[1,51], [2,51], [3,51], [4,51], [5,51], [6,51], [7,51], [8,51],    [1,80]]  // Track Selectors inc Master
-		],
-		faders: // AG > These are all the Track controls by Channel in this order [Mute] [Solo] [A/B] [Record-Arm]
-        [ 
-			[[1,50], [1,66], [1,49], [1,48]], // Channel 1 [Mute] [Solo] [A/B] [Record-Arm]
-			[[2,50], [2,66], [2,49], [2,48]], // Channel 2 [Mute] [Solo] [A/B] [Record-Arm]
-			[[3,50], [3,66], [3,49], [3,48]], // Channel 3 [Mute] [Solo] [A/B] [Record-Arm]
-			[[4,50], [4,66], [4,49], [4,48]], // Channel 4 [Mute] [Solo] [A/B] [Record-Arm]
-			[[5,50], [5,66], [5,49], [5,48]], // Channel 5 [Mute] [Solo] [A/B] [Record-Arm]
-			[[6,50], [6,66], [6,49], [6,48]], // Channel 6 [Mute] [Solo] [A/B] [Record-Arm]
-			[[7,50], [7,66], [7,49], [7,48]], // Channel 7 [Mute] [Solo] [A/B] [Record-Arm]
-			[[8,50], [8,66], [8,49], [8,48]]  // Channel 8 [Mute] [Solo] [A/B] [Record-Arm]
-		]
-	},
-	buttons: {
-		top:                                  // AG > Buttons Top Block
-        { 
-			"pan":          [1, 87],
-			"sends":        [1, 88],
-			"user":         [1, 89],
-
-			"play":         [1, 91],
-			"record":       [1, 93],
-			"session":      [1,102],
-
-			"metronome":    [1, 90],
-			"tapTempo":     [1, 99],
-			"nudgeMinus":   [1,100],
-			"nudgePlus":    [1,101],
-		},
-
-		encoders: // AG > Buttons Bottom Block (need renaming)
-        { 
-			"1DevicePrev":  [1, 58],
-			"2DeviceNext":  [1, 59],
-			"3BankPrev":    [1, 60],
-			"4BankNext":    [1, 61],
-			"5DevOnOff":    [1, 62],
-			"6DevLock":     [1, 63],
-			"7ClipDevView": [1, 64],
-			"8DetailView":  [1, 65],
-
-			"shift":        [1, 98],
-			"bank":         [1,103]
-		},
-
-		bankSelect: // AG >  Bank Select Different Order than I have in JSON - TAKE CARE
-        { 
-			up:     [1,94], // WHY NO "" ?
-			down:   [1,95],
-			left:   [1,97],
-			right:  [1,96]
-		}
-	},
-};
-
-*/
-
-/* ORIGINAL CC VARIABLE
-var ccNotes = {
-	encoders:                                                   // AG > This is the Knobs - NAME CHANGE NEEDED
-    { 
-		top: [[1,48], [1,49], [1,50], [1,51], [1,52], [1,53], [1,54], [1,55]],
-
-
-		side:
-        [
-            [[1,16], [1,17], [1,18], [1,19], [1,20], [1,21], [1,22], [1,23]], // All Modes
-            [[2,16], [2,17], [2,18], [2,19], [2,20], [2,21], [2,22], [2,23]], // Generic Mode (0) ONLY
-            [[3,16], [3,17], [3,18], [3,19], [3,20], [3,21], [3,22], [3,23]], // Generic Mode (0) ONLY
-            [[4,16], [4,17], [4,18], [4,19], [4,20], [4,21], [4,22], [4,23]], // Generic Mode (0) ONLY
-            [[5,16], [5,17], [5,18], [5,19], [5,20], [5,21], [5,22], [5,23]], // Generic Mode (0) ONLY
-            [[6,16], [6,17], [6,18], [6,19], [6,20], [6,21], [6,22], [6,23]], // Generic Mode (0) ONLY
-            [[7,16], [7,17], [7,18], [7,19], [7,20], [7,21], [7,22], [7,23]], // Generic Mode (0) ONLY
-            [[8,16], [8,17], [8,18], [8,19], [8,20], [8,21], [8,22], [8,23]], // Generic Mode (0) ONLY
-            [[9,16], [9,17], [9,18], [9,19], [9,20], [9,21], [9,22], [9,23]], // Generic Mode (0) ONLY
-        ],
-
-		cue: [1,47],                                            // AG > The actual Rotary Encoders
-		tempo: [1,13]
-	},                                                          // AG > Th faders inc Master and Cross
-	faders:                                                     // CHECK BRACKETS HERE
-    [
-        [1,7], [2,7], [3,7], [4,7], [5,7], [6,7], [7,7], [8,7], [1,14], [1,15]
-    ]
-};
-
-*/
-
-// AG > This is all the names of the buttons that light up
-// var buttonLedKeys = ["pan","sends","user","play","record","session","metronome", "1DevicePrev", "2DeviceNext", "3BankPrev", "4BankNext", "5DevOnOff", "6DevLock", "7ClipDevView", "8DetailView", "bank"];
-
-var noteValueObj = [[],[],[],[],[],[],[],[],[]]; // AG > EMPTY ARRAY DECLARATION FOR MIDI MESSAGES ???
-
-var ccValueObj = [[],[],[],[],[],[],[],[],[]]; // AG > EMPTY ARRAY DECLARATION FOR MIDI MESSAGES ???
-
-var colorParameterObj = [[],[],[],[],[]]; // AG > EMPTY ARRAY DECLARATION FOR MIDI MESSAGES ???
-
-var modeParameterObj = [[],[],[],[],[]]; // AG > EMPTY ARRAY DECLARATION FOR MIDI MESSAGES ???
-
-var faderPadParameterObj = [[],[],[],[],[],[],[],[]]; // AG > EMPTY ARRAY DECLARATION FOR MIDI MESSAGES ???
-
-var buttonParameterObj = {}; // AG > WHAT'S THIS FOR???
 
 // GLOBAL VARIABLES END //////////////////////
 
 // MAIN //////////////////////////////////////
 
-function init ()
-{
-	
+
+function init() {
+    local.parameters.pass_through.setCollapsed(true);
+    local.values.infos.setCollapsed(true);
+    local.values.tempo.setCollapsed(true);
+    local.values.mtc.setCollapsed(true);
+    local.scripts.setCollapsed(true);
+    local.commandTester.setCollapsed(true);
+    local.templates.setCollapsed(true);
 };
 
 function setRingModeSingle(param, noteIndex, chanIndex) {
